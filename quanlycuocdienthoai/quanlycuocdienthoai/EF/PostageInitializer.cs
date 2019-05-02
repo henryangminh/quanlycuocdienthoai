914ad2f0de2e5b83d5c44bf1d3a8267fdee64dc6
Hồ Trường Nhật Minh <hippore114@gmail.com>
2019-05-03 02:40:11 +0700

    Add more Initializer

--- quanlycuocdienthoai/quanlycuocdienthoai/EF/PostageInitializer.cs
+++ quanlycuocdienthoai/quanlycuocdienthoai/EF/PostageInitializer.cs
@@ -15,7 +15,8 @@ namespace quanlycuocdienthoai.EF
                 context.Customers.AddRange(new List<Customer>()
                 {
                     new Customer(){ CustomerName="Bé Yêu", Address="Số 1 Đường Bé Yêu", DateRegistered=new DateTime(2019,4,1), Email="hippore114@gmail.com", CMND="069123412" },
-                    new Customer(){ CustomerName="Gấu", Address="Số 3 Đường Bé Yêu", DateRegistered=new DateTime(2019,5,1), Email="hotruongnhatminh@gmail.com", CMND="025861398" }
+                    new Customer(){ CustomerName="Gấu", Address="Số 3 Đường Bé Yêu", DateRegistered=new DateTime(2019,5,1), Email="hotruongnhatminh@gmail.com", CMND="025861398" },
+                    new Customer(){ CustomerName="TL", Address="Đoán xem", DateRegistered=new DateTime(2019,6,1), Email="tltltl@gmail.com", CMND="013929123" }
                 });
             }
             context.SaveChanges();
@@ -27,6 +28,9 @@ namespace quanlycuocdienthoai.EF
                     new PhoneNumber() { PhoneNo="0919991167", Status=false},
                     new PhoneNumber() { PhoneNo="0984861168", Status=false},
                     new PhoneNumber() { PhoneNo="0523188169", Status=false},
+                    new PhoneNumber() { PhoneNo="0819283912", Status=false},
+                    new PhoneNumber() { PhoneNo="0220122221", Status=false},
+                    new PhoneNumber() { PhoneNo="0234128399", Status=false},
                 });
             }
             context.SaveChanges();
@@ -36,6 +40,13 @@ namespace quanlycuocdienthoai.EF
                 context.SIMs.AddRange(new List<SIM>()
                 {
                     new SIM(){ PhoneNumberFK=1, Status = true},
+                    new SIM(){ PhoneNumberFK=null, Status = false},
+                    new SIM(){ PhoneNumberFK=null, Status = false},
+                    new SIM(){ PhoneNumberFK=null, Status = false},
+                    new SIM(){ PhoneNumberFK=null, Status = false},
+                    new SIM(){ PhoneNumberFK=null, Status = false},
+                    new SIM(){ PhoneNumberFK=null, Status = false},
+                    new SIM(){ PhoneNumberFK=null, Status = false},
                     new SIM(){ PhoneNumberFK=null, Status = false},
                     new SIM(){ PhoneNumberFK=null, Status = false},
                     new SIM(){ PhoneNumberFK=null, Status = false}
